2f6a7203042e2b252e959f07d1ee64e2671d7e37
Kyle <kylejmbaylis@gmail.com>
2022-07-13 15:15:32 -0400

    layout for portfolio done

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,8 +1,78 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function Portfolio() {
     return (
-        <h2>My Portfolio</h2>
+        <div>
+            <h2>My Portfolio</h2>
+            <Container>
+                <Row>
+                    <Col>
+                        <Card>
+                            <Card.Img src={require(`../../assets/images/egg.jpg`)} />
+                            <Card.Body>
+                                <Card.Title>Project</Card.Title>
+                                <Button>Link</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card>
+                            <Card.Img src={require(`../../assets/images/egg.jpg`)} />
+                            <Card.Body>
+                                <Card.Title>Project</Card.Title>
+                                <Button>Link</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Card>
+                            <Card.Img src={require(`../../assets/images/egg.jpg`)} />
+                            <Card.Body>
+                                <Card.Title>Project</Card.Title>
+                                <Button>Link</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card>
+                            <Card.Img src={require(`../../assets/images/egg.jpg`)} />
+                            <Card.Body>
+                                <Card.Title>Project</Card.Title>
+                                <Button>Link</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Card>
+                            <Card.Img src={require(`../../assets/images/egg.jpg`)} />
+                            <Card.Body>
+                                <Card.Title>Project</Card.Title>
+                                <Button>Link</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card>
+                            <Card.Img src={require(`../../assets/images/egg.jpg`)} />
+                            <Card.Body>
+                                <Card.Title>Project</Card.Title>
+                                <Button>Link</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+
     );
 }
 
