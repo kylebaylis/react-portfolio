c78df50047020ed45251c81c9d1da4a4649d6e47
Kyle <kylejmbaylis@gmail.com>
2022-07-15 14:00:15 -0400

    portfolio links

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -15,9 +15,9 @@ function Portfolio() {
                         <Card className='card'>
                             <Card.Img src={require(`../../assets/images/egg.jpg`)} />
                             <Card.Body>
-                                <Card.Title>Project</Card.Title>
-                                <Button>Application</Button>
-                                <Button>GitHub</Button>
+                                <Card.Title>Run Buddy</Card.Title>
+                                <Button><a className='portbtn' href="https://kylebaylis.github.io/run-buddy/" target="_blank" rel="noreferrer">Application</a></Button>
+                                <Button><a className='portbtn' href="https://github.com/kylebaylis/run-buddy" target="_blank" rel="noreferrer">GitHub</a></Button>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -25,31 +25,9 @@ function Portfolio() {
                         <Card className='card'>
                             <Card.Img src={require(`../../assets/images/egg.jpg`)} />
                             <Card.Body>
-                                <Card.Title>Project</Card.Title>
-                                <Button>Application</Button>
-                                <Button>GitHub</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Card className='card'>
-                            <Card.Img src={require(`../../assets/images/egg.jpg`)} />
-                            <Card.Body>
-                                <Card.Title>Project</Card.Title>
-                                <Button>Application</Button>
-                                <Button>GitHub</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card className='card'>
-                            <Card.Img src={require(`../../assets/images/egg.jpg`)} />
-                            <Card.Body>
-                                <Card.Title>Project</Card.Title>
-                                <Button>Application</Button>
-                                <Button>GitHub</Button>
+                                <Card.Title>Password Generator</Card.Title>
+                                <Button><a className='portbtn' href="https://kylebaylis.github.io/password-generator/" target="_blank" rel="noreferrer">Application</a></Button>
+                                <Button><a className='portbtn' href="https://github.com/kylebaylis/password-generator" target="_blank" rel="noreferrer">GitHub</a></Button>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -59,9 +37,9 @@ function Portfolio() {
                         <Card className='card'>
                             <Card.Img src={require(`../../assets/images/egg.jpg`)} />
                             <Card.Body>
-                                <Card.Title>Project</Card.Title>
-                                <Button>Application</Button>
-                                <Button>GitHub</Button>
+                                <Card.Title>Code Quiz</Card.Title>
+                                <Button><a className='portbtn' href="https://kylebaylis.github.io/code-quiz/" target="_blank" rel="noreferrer">Application</a></Button>
+                                <Button><a className='portbtn' href="https://github.com/kylebaylis/code-quiz" target="_blank" rel="noreferrer">GitHub</a></Button>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -69,9 +47,31 @@ function Portfolio() {
                         <Card className='card'>
                             <Card.Img src={require(`../../assets/images/egg.jpg`)} />
                             <Card.Body>
-                                <Card.Title>Project</Card.Title>
-                                <Button>Application</Button>
-                                <Button>GitHub</Button>
+                                <Card.Title>Workday Scheduler</Card.Title>
+                                <Button><a className='portbtn' href="https://kylebaylis.github.io/workday-scheduler/" target="_blank" rel="noreferrer">Application</a></Button>
+                                <Button><a className='portbtn' href="https://github.com/kylebaylis/workday-scheduler" target="_blank" rel="noreferrer">GitHub</a></Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Card className='card'>
+                            <Card.Img src={require(`../../assets/images/egg.jpg`)} />
+                            <Card.Body>
+                                <Card.Title>Weather Dashboard</Card.Title>
+                                <Button><a className='portbtn' href="https://kylebaylis.github.io/Challenge6-WeatherDash/" target="_blank" rel="noreferrer">Application</a></Button>
+                                <Button><a className='portbtn' href="https://github.com/kylebaylis/Challenge6-WeatherDash" target="_blank" rel="noreferrer">GitHub</a></Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card className='card'>
+                            <Card.Img src={require(`../../assets/images/egg.jpg`)} />
+                            <Card.Body>
+                                <Card.Title>Budget Tracker</Card.Title>
+                                <Button><a className='portbtn' href="https://gentle-basin-85791.herokuapp.com/" target="_blank" rel="noreferrer">Application</a></Button>
+                                <Button><a className='portbtn' href="https://github.com/kylebaylis/PWA-Budget-Tracker" target="_blank" rel="noreferrer">GitHub</a></Button>
                             </Card.Body>
                         </Card>
                     </Col>
